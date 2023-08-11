5c7a982716ab82f6b1511a434a69e9dc59cbb56b
agfline <dev.agfline@posteo.net>
2023-08-11 23:39:11 +0200

    update libaaf to https://github.com/agfline/LibAAF/commit/7e10f697bff7b4cb259f8ae85180643f69669250

--- libs/aaf/aaf/version.h
+++ libs/aaf/aaf/version.h
@@ -2,5 +2,5 @@
 /* Ardour build */
 #ifndef LIBAAF_VERSION_H_
 #define LIBAAF_VERSION_H_
-#define LIBAAF_VERSION "v0.1-66-g4353854"
+#define LIBAAF_VERSION "v0.1-67-g7e10f69"
 #endif  /* LIBAAF_VERSION_H_ */
